bdd1791020d84ad15f03571079ef91dde60b7397
simpsonpaisley <simpsonpaisley@gmail.com>
2023-05-18 18:41:42 +0100

    Add set up files for server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [fortune, setFortune] = useState('');
+	async function fortuneHandler() {
+		const API = 'http://localhost:2021/random';
+
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+	return (
+		<div className="App">
+			<h1>Cookies</h1>
+			<button onClick={fortuneHandler}>Get a Random Fortune</button>
+			<p>{fortune}</p>
+		</div>
+	);
 }
 
 export default App;
