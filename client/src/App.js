d54c3f7c8dc06eadf9fb560e3b4f79e817aa6205
simpsonpaisley <simpsonpaisley@gmail.com>
2023-05-18 21:01:30 +0100

    Add random fortune from a category functionality in client

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,46 @@ import Axios from 'axios';
 import { useState } from 'react';
 
 function App() {
-	const [fortune, setFortune] = useState('');
+	const [fortune, setFortune] = useState({});
 	async function fortuneHandler() {
 		const API = 'http://localhost:2021/random';
 
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+
+	async function luckHandler() {
+		const API = 'http://localhost:2021/random/luck';
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+
+	async function quoteHandler() {
+		const API = 'http://localhost:2021/random/quotes';
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+
+	async function futureHandler() {
+		const API = 'http://localhost:2021/random/future';
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+
+	async function loveHandler() {
+		const API = 'http://localhost:2021/random/love';
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+
+	async function comedyHandler() {
+		const API = 'http://localhost:2021/random/comedy';
+		const response = await Axios.get(API);
+		setFortune(response.data);
+	}
+
+	async function techHandler() {
+		const API = 'http://localhost:2021/random/tech';
 		const response = await Axios.get(API);
 		setFortune(response.data);
 	}
@@ -14,7 +50,13 @@ function App() {
 		<div className="App">
 			<h1>Cookies</h1>
 			<button onClick={fortuneHandler}>Get a Random Fortune</button>
-			<p>{fortune}</p>
+			<button onClick={luckHandler}>Get a Random Luck Fortune</button>
+			<button onClick={quoteHandler}>Get a Random Quote Fortune</button>
+			<button onClick={futureHandler}>Get a Random Future Fortune</button>
+			<button onClick={loveHandler}>Get a Random Love Fortune</button>
+			<button onClick={comedyHandler}>Get a Random Comedy Fortune</button>
+			<button onClick={techHandler}>Get a Random Tech Fortune</button>
+			<p>{fortune.fortune}</p>
 		</div>
 	);
 }
--- server/server.js
+++ server/server.js
@@ -1,59 +1,65 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
-const bp = require("body-parser");
+const bp = require('body-parser');
 
-const Cookie = require("./models/cookie");
+const Cookie = require('./models/cookie');
 
 app.use(bp.json());
 
 // ---- GET ----
 
-app.get("/", (request, response) => {
-  response.json("You are on the root of the Fortune Cookies Server");
+app.get('/', (request, response) => {
+	response.json('You are on the root of the Fortune Cookies Server');
 });
 
-app.get("/cookies", async (request, response) => {
-  const cookies = await Cookie.find({});
-  response.json(cookies);
+app.get('/cookies', async (request, response) => {
+	const cookies = await Cookie.find({});
+	response.json(cookies);
 });
 
-app.get("/cookie/:id", async (request, response) => {
-  const gotCookie = await Cookie.findById(request.params.id);
-  response.json(gotCookie);
+app.get('/cookie/:id', async (request, response) => {
+	const gotCookie = await Cookie.findById(request.params.id);
+	response.json(gotCookie);
 });
 
-app.get("/cookies/:category", async (request, response) => {
-  const cookies = await Cookie.find(request.params);
-  response.json(cookies);
+app.get('/cookies/:category', async (request, response) => {
+	const cookies = await Cookie.find(request.params);
+	response.json(cookies);
 });
 
-app.get("/random", async (request, response) => {
-  const cookies = await Cookie.find({});
-  const random = Math.floor(Math.random() * cookies.length);
-  response.json(cookies[random]);
+app.get('/random', async (request, response) => {
+	const cookies = await Cookie.find({});
+	const random = Math.floor(Math.random() * cookies.length);
+	response.json(cookies[random]);
+});
+
+app.get('/random/:category', async function (request, response) {
+	const cookies = await Cookie.find(request.params);
+	const random = Math.floor(Math.random() * cookies.length);
+	response.json(cookies[random]);
 });
 
 // ---- POST ----
 
-app.post("/cookie", async function (request, response) {
-  console.log(request.body);
-  const newCookie = await Cookie.create(request.body);
-  response.json(newCookie);
+app.post('/cookie', async function (request, response) {
+	console.log(request.body);
+	const newCookie = await Cookie.create(request.body);
+	response.json(newCookie);
 });
 
 // ---- DELETE ----
 
-app.delete("/cookie/:id", async (request, response) => {
-  const deletedCookie = await Cookie.findByIdAndDelete(request.params.id);
-  response.json(deletedCookie);
+app.delete('/cookie/:id', async (request, response) => {
+	const deletedCookie = await Cookie.findByIdAndDelete(request.params.id);
+	response.json(deletedCookie);
 });
 
 app.listen(PORT, () => {
-  console.log("LISTENING ON PORT " + PORT);
+	console.log('LISTENING ON PORT ' + PORT);
 });
